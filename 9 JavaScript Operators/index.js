70cdab6e936a1290e80926c2716aeb3ea838b865
Shakil57375 <shakil57375@gmail.com>
2023-08-12 12:48:26 +0600

    JavaScript Operators

---
+++ 9 JavaScript Operators/index.js
@@ -0,0 +1,15 @@
+/* 
+Operator	Example	    Same As
+=	        x = y	    x = y
++=	        x += y	    x = x + y
+-=	        x -= y	    x = x - y
+*=	        x *= y	    x = x * y
+/=	        x /= y	    x = x / y
+%=	        x %= y	    x = x % y
+**=	        x **= y	    x = x ** y
+*/
+
+let x = 10;
+x += 5; // x = x + 5
+console.log(x); // output = 15
+
