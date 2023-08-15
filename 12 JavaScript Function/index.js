d1265a460cefda8be12477fa28c1a458c67b02b8
Shakil57375 <shakil57375@gmail.com>
2023-08-15 09:32:36 +0600

    javaScript Function

---
+++ 12 JavaScript Function/index.js
@@ -0,0 +1,40 @@
+/* function sleep(name, doing){ //parameter
+    console.log(name + " is " + doing + " now ");
+}
+sleep("Mah", "sleep")
+sleep("Shakil", "scrolling Facebook") //arguments
+sleep("Fa", "Going")
+ */
+
+// function myFunction(a, b){
+//     console.log("object");
+//     return a * b;
+//     // return undefined // by default javaScript returns undefined.
+// }
+
+// let x = myFunction(4, 3)
+// console.log(x);
+
+// function toCelsius(fahrenheit){
+//     return (5/9) * (fahrenheit - 32)
+// }
+
+// console.log(toCelsius(72));
+
+// function toCelsius(fahrenheit){
+//     return (5/9) * (fahrenheit - 32)
+// }
+
+// console.log(toCelsius);// if we didn't use () then it will return an object. By default everything in javascript is a object.
+
+
+let name = "hossain"
+function myFunc(){
+    let name  = "shakil"
+    console.log(name); //shakil
+}
+
+myFunc()
+
+console.log(name);// hossain
+// name will not be accessible here because it's a functional scope.
---
+++ 5 JavaScript Comments/index.js
@@ -0,0 +1,5 @@
+// this is a javascript single line comment
+/* 
+This is a javascript
+multiple line comment.
+ */
