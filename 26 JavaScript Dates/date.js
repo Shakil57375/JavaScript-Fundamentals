f1f5dae054cb5e2b7d7f75d3789cdb34195e13ca
Shakil57375 <shakil57375@gmail.com>
2023-09-08 20:26:54 +0600

    javaScritp date method added

---
+++ 26 JavaScript Dates/date.js
@@ -0,0 +1,10 @@
+const date = new Date()
+// console.log(date)
+// The getFullYear() Method
+// The getFullYear() method returns the year of a date as a four digit number:
+const y = new Date("2021-03-25");
+console.log(y.getFullYear());//2021
+// The getMonth() Method
+// The getMonth() method returns the month of a date as a number (0-11).
+const m = new Date();
+console.log(m.getMonth())// 8 The [getMonth() method returns the month of a date as a number (0-11)].
