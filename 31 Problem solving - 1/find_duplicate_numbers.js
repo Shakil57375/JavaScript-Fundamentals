5a4ec4a196613fb2acdc2c7a3509e953067e2bd4
Shakil57375 <shakil57375@gmail.com>
2023-09-13 14:40:48 +0600

    javascript booleans added

--- 31 Problem solving - 1/find_duplicate_numbers.js
+++ 31 Problem solving - 1/find_duplicate_numbers.js
@@ -7,11 +7,15 @@
 
 // console.log(duplicates)
 
-// কোন  Array থেকে unique নম্বর বের করার নিয়ম।
+// // কোন  Array থেকে unique নম্বর বের করার নিয়ম।
 
-const numbers = [1, 3, 2, 2, 14, 1, 4, 2, 3, 6, 8]
-const unique = numbers.filter((value, index, array)=>{
-    return array.indexOf(value) === index
-})
+// const numbers = [1, 3, 2, 2, 14, 1, 4, 2, 3, 6, 8]
+// const unique = numbers.filter((value, index, array)=>{
+//     // return array.indexOf(value) === index
+//     // console.log(array.indexOf(value))
+//     console.log(value)
+//     console.log(index)
+// })
 
-console.log(unique)
+// console.log(unique)
+  
