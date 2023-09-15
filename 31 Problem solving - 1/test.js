0e7621bdfb158dd240f99c604a457e6f08442701
Shakil57375 <shakil57375@gmail.com>
2023-09-15 14:59:21 +0600

    find longest word

--- 31 Problem solving - 1/test.js
+++ 31 Problem solving - 1/test.js
@@ -2,13 +2,14 @@ const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
 const findVowels = (sentence) =>{
     let count = 0;
-    const letters = Array.from(sentence)
-    letters.forEach((value, index, array)=>{
+    const split = sentence.split("")
+    // const array = Array.from(sentence)
+    split.filter((value, index, array)=>{
         if(vowels.includes(value)){
             count++
         }
     })
-    return count
+    return count++
 }
 
-console.log(findVowels("Hello"))
+console.log(findVowels("This is my place"))
--- 33 JavaScript Comparisons/comparing.js
+++ 33 JavaScript Comparisons/comparing.js
@@ -7,11 +7,21 @@
 // "2" < "12"	false
 // "2" > "12"	true
 // "2" == "12"	false
-let age = 14;
-age = Number(age)
-if(isNaN(age)){
-    console.log("input is not a number")
+// let age = 14;
+// age = Number(age)
+// if(isNaN(age)){
+//     console.log("input is not a number")
+// }
+// else{
+//     console.log(age < 18 ? "too young" : "Old enough" );
+// }
+
+let name = "22"
+name = String(name) 
+if(!isNaN(name)){
+    console.log(`${name}  is a number`)
 }
 else{
-    console.log(age < 18 ? "too young" : "Old enough" );
+    console.log(name === "Shakil" ? "I am Shakil" : "I don't know you!!")
 }
+
