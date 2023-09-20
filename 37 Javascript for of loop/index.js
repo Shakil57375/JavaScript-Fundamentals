2e3d21dafe16b02ad3c1b1529c166ba7d98578b8
Shakil57375 <shakil57375@gmail.com>
2023-09-20 19:39:26 +0600

    for of loop added

---
+++ 37 Javascript for of loop/index.js
@@ -0,0 +1,11 @@
+/* 
+The For Of Loop
+The JavaScript for of statement loops through the values of an iterable object.
+
+It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more:
+ */
+let x = ["Shakil", "s", "f"];
+
+for (let a of x) {
+  console.log(a);
+}
