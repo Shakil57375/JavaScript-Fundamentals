c4de5f311d339f93282139fbb07b571d61c7f69c
Shakil57375 <shakil57375@gmail.com>
2023-09-24 15:08:23 +0600

    method.size added

--- 42 JavaScript Maps/index.js
+++ 42 JavaScript Maps/index.js
@@ -9,17 +9,17 @@ forEach()	Invokes a callback for each key/value pair in a Map
 entries()	Returns an iterator object with the [key, value] pairs in a Map
 keys()	Returns an iterator object with the keys in a Map
 values()	Returns an iterator object of the values in a Map */
-/* // Create a Map
+ // Create a Map
 const fruits = new Map([
   ["apples", 500],
   ["bananas", 300],
   ["oranges", 200],
 ]);
-console.log(fruits) */
+console.log(fruits.size) 
 
 
 // Create a Map
-const fruits = new Map();
+/* const fruits = new Map();
 
 // Set Map Values
 fruits.set("apples", 500);
@@ -29,3 +29,4 @@ fruits.set("oranges", 200);
 console.log(fruits.get("apples"))
 console.log(fruits.delete("oranges"))
 console.log(fruits)
+ */
