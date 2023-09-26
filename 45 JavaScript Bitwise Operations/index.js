60cb5542047b1c1178a3f26002b559f780e5dfcf
Shakil57375 <shakil57375@gmail.com>
2023-09-26 14:50:47 +0600

    JavaScript Bitwise operator added

---
+++ 45 JavaScript Bitwise Operations/index.js
@@ -0,0 +1,5 @@
+// converting decimal to binary
+let number = 20
+console.log((number).toString(2))
+// converting binary to decimal
+console.log((0b1001).toString(10)) // have to add ob in front of the binary number to let the browser identify the binary number  
