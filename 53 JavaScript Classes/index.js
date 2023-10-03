bad4ba1b6b311328eda334214d0743e76458654a
Shakil57375 <shakil57375@gmail.com>
2023-10-03 11:01:15 +0600

    javascript classes adding

---
+++ 53 JavaScript Classes/index.js
@@ -0,0 +1,28 @@
+class ClassMates{
+	constructor(name,age){
+    	this.name=name;
+      	this.age=age;
+    }
+  	displayInfo(){
+    	return this.name + "is " + this.age + " years old!";
+    }
+}
+
+let classmate = new ClassMates("Mike Will",15);
+classmate.displayInfo();  // result: Mike Will is 15 years old!
+
+class Car {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+    age(x) {
+        return x - this.year;
+    }
+}
+
+let date = new Date();
+let year = date.getFullYear();
+
+let myCar = new Car("Ford", 2014);
+document.getElementById("class1").innerHTML = "My car is " + myCar.age(year) + " years old.";
