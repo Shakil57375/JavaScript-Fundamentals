7d33ae5e4a2ed5f2e3953d9b3580569da7789131
Shakil57375 <shakil57375@gmail.com>
2023-10-08 11:26:27 +0600

    Declare at the beginning

---
+++ 57 JavaScript Best Practices/index.js
@@ -0,0 +1,11 @@
+// Declare at the beginning
+let firstName, lastName, price, discount, fullPrice;
+
+// Use later
+firstName = "John";
+lastName = "Doe";
+
+price = 19.90;
+discount = 0.10;
+
+fullPrice = price - discount;
