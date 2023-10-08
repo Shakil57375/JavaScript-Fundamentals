bff2b5b213bab6eee07dc6f0165f658c40b80e93
Shakil57375 <shakil57375@gmail.com>
2023-10-08 11:29:04 +0600

    Declare object with const

--- 57 JavaScript Best Practices/index.js
+++ 57 JavaScript Best Practices/index.js
@@ -9,12 +9,29 @@ price = 19.90;
 discount = 0.10;
 
 fullPrice = price - discount; */
+/* Initialize Variables
+It is a good coding practice to initialize variables when you declare them.
 
+This will:
+
+Give cleaner code
+Provide a single place to initialize variables
+Avoid undefined values */
 // Declare and initiate at the beginning
 let firstName = "";
 let lastName = "";
 let price = 0;
 let discount = 0;
-let fullPrice = 0,
+let fullPrice = 0;
 const myArray = [];
 const myObject = {};
+
+/* Declare Objects with const
+Declaring objects with const will prevent any accidental change of type: */
+
+// Example
+/* let car = {type:"Fiat", model:"500", color:"white"};
+car = "Fiat";      // Changes object to string
+
+const car = {type:"Fiat", model:"500", color:"white"};
+car = "Fiat";      // Not possible */
