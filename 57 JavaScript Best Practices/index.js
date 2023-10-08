4d09dda641eadffb0649acfb435c3ad53287f7cb
Shakil57375 <shakil57375@gmail.com>
2023-10-08 11:27:22 +0600

    Declare and initiate at the beginning

--- 57 JavaScript Best Practices/index.js
+++ 57 JavaScript Best Practices/index.js
@@ -1,4 +1,4 @@
-// Declare at the beginning
+/* // Declare at the beginning
 let firstName, lastName, price, discount, fullPrice;
 
 // Use later
@@ -8,4 +8,13 @@ lastName = "Doe";
 price = 19.90;
 discount = 0.10;
 
-fullPrice = price - discount;
+fullPrice = price - discount; */
+
+// Declare and initiate at the beginning
+let firstName = "";
+let lastName = "";
+let price = 0;
+let discount = 0;
+let fullPrice = 0,
+const myArray = [];
+const myObject = {};
