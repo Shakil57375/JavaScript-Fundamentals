e5538c70300a34023f1d59a59d17dd0c04b4307e
Shakil57375 <shakil57375@gmail.com>
2023-10-12 22:18:15 +0600

    good code sippet added

--- 59 JavaScript Perfomance/index.js
+++ 59 JavaScript Perfomance/index.js
@@ -3,7 +3,14 @@ const t0 = performance.now();
 const t1 = performance.now();
 console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
 
-// Bad:
-for (let i = 0; i < arr.length; i++) {
+// // Bad:
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(i)
+// }
+
+let arr = ["m", "s", "p"]
+// Better Code:
+let l = arr.length;
+for (let i = 0; i < l; i++) {
     console.log(i)
 }
