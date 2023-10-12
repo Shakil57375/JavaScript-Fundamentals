a5b8819669bc8a0d0ff3bb0e07256b2317c90acd
Shakil57375 <shakil57375@gmail.com>
2023-10-12 22:52:13 +0600

    Other Reserved Words added

--- 60 JavaScript Reserver Words/index.js
+++ 60 JavaScript Reserver Words/index.js
@@ -34,3 +34,29 @@ isPrototypeOf	length	Math	NaN
 name	Number	Object	prototype
 String	toString	undefined	valueOf
  */
+
+/* Other Reserved Words
+JavaScript can be used as the programming language in many applications.
+
+You should also avoid using the name of HTML and Window objects and properties:
+alert	all	anchor	anchors
+area	assign	blur	button
+checkbox	clearInterval	clearTimeout	clientInformation
+close	closed	confirm	constructor
+crypto	decodeURI	decodeURIComponent	defaultStatus
+document	element	elements	embed
+embeds	encodeURI	encodeURIComponent	escape
+event	fileUpload	focus	form
+forms	frame	innerHeight	innerWidth
+layer	layers	link	location
+mimeTypes	navigate	navigator	frames
+frameRate	hidden	history	image
+images	offscreenBuffering	open	opener
+option	outerHeight	outerWidth	packages
+pageXOffset	pageYOffset	parent	parseFloat
+parseInt	password	pkcs11	plugin
+prompt	propertyIsEnum	radio	reset
+screenX	screenY	scroll	secure
+select	self	setInterval	setTimeout
+status	submit	taint	text
+textarea	top	unescape	untaint */
