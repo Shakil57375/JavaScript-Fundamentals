680033fb363eb875c162ce26420d43486786f022
Shakil57375 <shakil57375@gmail.com>
2023-10-12 22:24:39 +0600

    Reduce DOM Access

--- 59 JavaScript Perfomance/index.js
+++ 59 JavaScript Perfomance/index.js
@@ -8,9 +8,18 @@ console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
 //     console.log(i)
 // }
 
-let arr = ["m", "s", "p"]
+let arr = ["m", "s", "p"];
 // Better Code:
 let l = arr.length;
 for (let i = 0; i < l; i++) {
-    console.log(i)
+  console.log(i);
 }
+
+/* Reduce DOM Access
+Accessing the HTML DOM is very slow, compared to other JavaScript statements.
+
+If you expect to access a DOM element several times, access it once, and use it as a local variable:
+
+Example
+const obj = document.getElementById("demo");
+obj.innerHTML = "Hello"; */
