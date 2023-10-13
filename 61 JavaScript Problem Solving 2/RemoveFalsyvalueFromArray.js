e3ad1e0b99be1803aa23e286e11a5b0f836ce807
Shakil57375 <shakil57375@gmail.com>
2023-10-13 11:38:59 +0600

    Remove Falsy value from the array and return the truthy values. one way added

---
+++ 61 JavaScript Problem Solving 2/RemoveFalsyvalueFromArray.js
@@ -0,0 +1,13 @@
+// Remove Falsy value from the array and return the truthy values.
+const truthyAndFalsy = [true, false, 1, 0, "Hello", "", {}, null, undefined];
+// way-1
+const trueArray = truthyAndFalsy.filter((el)=>{
+    console.log(el)
+    if(el){
+        return true
+    }else{
+        return false
+    }
+})
+
+console.log(trueArray)
