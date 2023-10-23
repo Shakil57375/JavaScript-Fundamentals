09c33b0ff87b1e2af0150007a160ad0cc20f37be
Shakil57375 <shakil57375@gmail.com>
2023-10-23 17:01:49 +0600

    set object added

--- 66 javascript object accessors/index.js
+++ 66 javascript object accessors/index.js
@@ -1,10 +1,26 @@
 //! Create an object:
-const person = {
+// ! get accessor
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     language: "en",
+//     get lang() {
+//       return this.language;
+//     }
+//   };
+//   console.log(person.lang)
+
+// ! set accessor
+const person2 = {
     firstName: "John",
     lastName: "Doe",
-    language: "en",
-    get lang() {
-      return this.language;
+    language: "",
+    set lang(lang) {
+      this.language = lang;
     }
   };
-  console.log(person.lang)
+
+  person2.lang = "bg"
+  console.log(person2)
+  
+  // Set an object property using a
