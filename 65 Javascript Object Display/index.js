9d967432edfb33d1e2afb28c241bef475c28c8aa
Shakil57375 <shakil57375@gmail.com>
2023-10-23 15:48:34 +0600

    function converts into strings

--- 65 Javascript Object Display/index.js
+++ 65 Javascript Object Display/index.js
@@ -45,3 +45,26 @@ const arr = ["John", "Peter", "Sally", "Jane"];
 let myString2 = JSON.stringify(arr);
 document.getElementById("demo").innerHTML = myString2;
 console.log(typeof myString2)
+
+// Stringify Functions
+// JSON.stringify will not stringify functions:
+
+// Example
+const person5 = {
+  name: "John",
+  age: function () {return 30;}
+};
+
+let myString5 = JSON.stringify(person5);
+document.getElementById("demo").innerHTML = myString5;
+// This can be "fixed" if you convert the functions into strings before stringifying.
+
+// Example
+const person6 = {
+  name: "John",
+  age: function () {return 30;}
+};
+person6.age = person6.age.toString();
+
+let myString8 = JSON.stringify(person6);
+document.getElementById("demo").innerHTML = myString8;
