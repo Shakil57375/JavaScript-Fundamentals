16a4a641fb40c96a098b7b0d860082ea60d820a3
Shakil57375 <shakil57375@gmail.com>
2023-10-23 15:41:54 +0600

    object values converted to array

--- 65 Javascript Object Display/index.js
+++ 65 Javascript Object Display/index.js
@@ -4,8 +4,13 @@ const person = {
     city: "New York"
   };
 
-  document.getElementById("demo").innerHTML = person;
-
-  for(elements in person){
-    console.log(person[elements])
+  const personArray = Object.values(person)
+  for(let values of personArray){
+    console.log(values)
   }
+
+//   document.getElementById("demo").innerHTML = person;
+
+//   for(elements in person){
+//     console.log(person[elements])
+//   }
