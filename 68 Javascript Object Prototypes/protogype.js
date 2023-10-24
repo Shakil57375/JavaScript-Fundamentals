8e61c5ba8c8b7bb70f76b948be80d4c8dc994b2e
Shakil57375 <shakil57375@gmail.com>
2023-10-24 17:25:07 +0600

    function added

--- 68 Javascript Object Prototypes/protogype.js
+++ 68 Javascript Object Prototypes/protogype.js
@@ -1,28 +1,32 @@
-/* function Person(name, age) {
-  let person = {};
-
+const personMethods = {
+  eat() {
+    console.log(`the person is eating`);
+  },
+  sleep() {
+    console.log(`the person is sleeping`);
+  },
+  play() {
+    console.log(`the person is playing`);
+  },
+};
+function Person(name, age) {
+  let person = Object.create(personMethods);
+  console.log(person);
   person.name = name;
   person.age = age;
-  person.eat = () => {
-    console.log(`person is eating`);
-  };
+  return person;
+}
 
-  person.sleep = function () {
-    console.log(`person is sleeping`);
-  };
-  return person
-};
-
-const sakib = Person("sakib", 33)
-const tamim = Person("tamim", 36) */
-
+const sakib = Person("sakib", 33);
+const tamim = Person("tamim", 36);
+sakib.eat()
 // creating a child blank object
 
 const captain = {
-    name : "Mash",
-    age : 33,
-    country : "Bangladesh"
-}
+  name: "Mash",
+  age: 33,
+  country: "Bangladesh",
+};
 
-const player = Object.create(captain)
-console.log(player.name)
+const player = Object.create(captain);
+console.log(player.name);
