3cf3ef4e29fc4e0772f2110bd85c57a6c6268a0e
Shakil57375 <shakil57375@gmail.com>
2023-10-24 19:04:37 +0600

    this keyword added

--- 68 Javascript Object Prototypes/protogype.js
+++ 68 Javascript Object Prototypes/protogype.js
@@ -1,4 +1,42 @@
-const personMethods = {
+// ? what is prototype?
+// * prototype is javascript function property which denotes an object
+// ! function without new keyword
+/* function Person(name, age) {
+  let person = Object.create(Person.prototype);
+  console.log(person);
+  person.name = name;
+  person.age = age;
+  return person;
+} */
+// object prototype
+/* Person.prototype = {
+  eat() {
+    console.log(`the person is eating`);
+  },
+  sleep() {
+    console.log(`the person is sleeping`);
+  },
+  play() {
+    console.log(`the person is playing`);
+  },
+}; */
+
+/* const sakib = new  Person("sakib", 33);
+const tamim = new Person("tamim", 36); */
+
+// ! function with a new keyword
+ 
+function Person(name, age){
+  // image the comments 
+  // just image if u use constructor function then it will happen.
+  // let this = Object.create(Person.prototype)
+    this.name = name;
+    this.age = age;
+  // return this;
+  
+}
+
+Person.prototype = {
   eat() {
     console.log(`the person is eating`);
   },
@@ -9,19 +47,16 @@ const personMethods = {
     console.log(`the person is playing`);
   },
 };
-function Person(name, age) {
-  let person = Object.create(personMethods);
-  console.log(person);
-  person.name = name;
-  person.age = age;
-  return person;
-}
 
-const sakib = Person("sakib", 33);
-const tamim = Person("tamim", 36);
-sakib.eat()
+const sakib = new  Person("sakib", 33);
+sakib.play()
+const tamim = new Person("tamim", 36);
+tamim.eat()
+const shakil = new Person("shakil", 21)
+
+
+
 // creating a child blank object
-
 const captain = {
   name: "Mash",
   age: 33,
