995625b623239df6e82a891d6c135fd77e223b2f
Shakil57375 <shakil57375@gmail.com>
2023-10-24 16:00:19 +0600

    how to add method added

--- 67 javascript object constructor/index.js
+++ 67 javascript object constructor/index.js
@@ -1,4 +1,4 @@
-// can't add any elements on the object template
+// We also learned that you can not add a new property to an existing object constructor:
 class Person {
     constructor(first, last, age) {
         this.firstName = first;
@@ -11,7 +11,14 @@ class Person {
 
 const shakil = new Person("shakil", "hosssain")
 const shakil2 = new Person("shakil2", "hosssain2")
+// Adding a new property to an existing object is easy:
 shakil.country = "BD"
+// Adding a new property to an existing object is easy:
 shakil2.age = 21
-console.log(shakil)
+// Adding a new method to an existing object is easy:
+shakil.profession = function () {
+    return `${this.firstName} is a student`;
+};
+
+console.log(shakil.profession())
 console.log(shakil2.fullName())
