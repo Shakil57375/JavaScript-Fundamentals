0a41c2363954617c7f7a15ad08fea3ee3ee1ee13
Shakil57375 <shakil57375@gmail.com>
2023-10-24 13:07:10 +0600

    constructor function template added

--- 67 javascript object constructor/index.js
+++ 67 javascript object constructor/index.js
@@ -1,3 +1,11 @@
-const sumit = {
-    
+class Person {
+    constructor(first, last, age) {
+        this.firstName = first;
+        this.lastName = last;
+    }
 }
+
+const sumit = new Person("shakil", "hosssain")
+const sumit1 = new Person("shakil2", "hosssain2")
+console.log(sumit)
+console.log(sumit1.firstName)
