22fcc35383d31bb230839b3fb397c592c0ee55ab
Shakil57375 <shakil57375@gmail.com>
2023-10-24 16:41:40 +0600

    javascript prototype added

--- 68 Javascript Object Prototypes/index.js
+++ 68 Javascript Object Prototypes/index.js
@@ -1,3 +1,4 @@
+// The JavaScript prototype property allows you to add new properties to object constructors:
 class Person {
     constructor(first, last, age, eyecolor) {
         this.firstName = first;
@@ -6,6 +7,15 @@ class Person {
         this.eyeColor = eyecolor;
     }
 }
+
+// The JavaScript prototype property allows you to add new properties to object constructors:
+Person.prototype.nationality = "Bangladeshi";
+// The JavaScript prototype property also allows you to add new methods to objects constructors:
+
+Person.prototype.name = function() {
+  return this.firstName + " " + this.lastName;
+};
   
   const myFather = new Person("John", "Doe", 50, "blue");
   const myMother = new Person("Sally", "Rally", 48, "green");
+  console.log(myFather.nationality)
