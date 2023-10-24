6d52ffd139cad195813ed3789badb91f24ea774b
Shakil57375 <shakil57375@gmail.com>
2023-10-24 17:07:33 +0600

    object added

---
+++ 68 Javascript Object Prototypes/protogype.js
@@ -0,0 +1,19 @@
+/* function Person(name, age) {
+  let person = {};
+
+  person.name = name;
+  person.age = age;
+  person.eat = () => {
+    console.log(`person is eating`);
+  };
+
+  person.sleep = function () {
+    console.log(`person is sleeping`);
+  };
+  return person
+};
+
+const sakib = Person("sakib", 33)
+const tamim = Person("tamim", 36) */
+
+// 
