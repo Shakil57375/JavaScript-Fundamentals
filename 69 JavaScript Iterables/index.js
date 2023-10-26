540ca168e38a8806a92fc7a4c9145f290ae11aba
Shakil57375 <shakil57375@gmail.com>
2023-10-26 11:37:51 +0600

    iterator added

--- 69 JavaScript Iterables/index.js
+++ 69 JavaScript Iterables/index.js
@@ -1,4 +1,13 @@
-const numbers = [1,2,3,4,5,6]
-for(let i = 0; i < numbers.length; i++){
-    console.log(numbers[i])
+const nums = [1,2,3]
+const numbers = {
+    one : 1,
+    two : 2,
+    three: 3
 }
+
+const numIterator = nums[Symbol.iterator]()
+console.log(numIterator.next())
+console.log(numIterator.next())
+console.log(numIterator.next())
+console.log(numIterator.next())
+
