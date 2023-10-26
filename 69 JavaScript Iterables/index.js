3036b60725226b60208b82ea23b68f5bd48d008d
Shakil57375 <shakil57375@gmail.com>
2023-10-26 12:01:25 +0600

    example added

--- 69 JavaScript Iterables/index.js
+++ 69 JavaScript Iterables/index.js
@@ -1,16 +1,39 @@
-const nums = [1,2,3]
+const nums = [1, 2, 3];
 const numbers = {
-    one : 1,
-    two : 2,
-    three: 3
-}
+  one: 1,
+  two: 2,
+  three: 3,
+};
 
-const numIterator = nums[Symbol.iterator]()
-console.log(numIterator.next())
-console.log(numIterator.next())
-console.log(numIterator.next())
-console.log(numIterator.next())
+const numIterator = nums[Symbol.iterator]();
+console.log(numIterator.next());
+console.log(numIterator.next());
+console.log(numIterator.next());
+console.log(numIterator.next());
 
 // ! What is iterator?
 // An Iterator is an object that has a property called symbol.iterator. By calling symbol.iterator we get an iterator and by calling its next function we can do looping.
 
+// Bestially, a object is not iterable with this object we can make a object not iterable to iterable
+
+const myNumbers = {};
+
+myNumbers[Symbol.iterator] = function () {
+  let n = 0;
+  let done = false;
+  return {
+    next() {
+      n += 10;
+      if (n == 100) {
+        done == true;
+      }
+      return {
+        value: n,
+        done: done,
+      };
+    },
+  };
+};
+
+for (let num of myNumbers){
+}
