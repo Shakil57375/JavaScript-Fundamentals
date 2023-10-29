cdaa3b585149c7111d4e9850c2c0199e519a32a3
Shakil57375 <shakil57375@gmail.com>
2023-10-29 16:14:11 +0600

    funciton invocation addded

--- 74 JavaScript Function Invocation/index.js
+++ 74 JavaScript Function Invocation/index.js
@@ -1,5 +1,16 @@
+/* Invoking a JavaScript Function
+The code inside a function is not executed when the function is defined.
+
+The code inside a function is executed when the function is invoked.
+
+It is common to use the term "call a function" instead of "invoke a function".
+
+It is also common to say "call upon a function", "start a function", or "execute a function".
+
+In this tutorial, we will use invoke, because a JavaScript function can be invoked without being called. */
 // we can call a function in two ways:
 // 1: by using the window object
+// * Invocation means to denote by whom the function is called
 // ! not recommended
 function withWindow(){
     console.log("I am the function with window object")
@@ -25,3 +36,18 @@ const x = {
 }
 
 x.a()
+
+
+// This is a function constructor:
+class myFunction {
+    constructor(arg1, arg2) {
+        this.firstName = arg1
+        this.lastName = arg2
+    }
+}
+  
+  // This creates a new object
+  const myObj = new myFunction("John", "Doe");
+  
+  // This will return "John"
+  myObj.firstName;
