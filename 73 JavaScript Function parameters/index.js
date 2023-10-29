4ac0cfe4de582687ee1df34a6bdbd31ec5765bce
Shakil57375 <shakil57375@gmail.com>
2023-10-29 12:18:23 +0600

    find max number added

--- 73 JavaScript Function parameters/index.js
+++ 73 JavaScript Function parameters/index.js
@@ -9,3 +9,16 @@ function a(x, y = 3) {
 }
 
 console.log(a(3));
+
+x = findMax(1, 123, 500, 115, 44, 88);
+
+function findMax() {
+  let max = -Infinity;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] > max) {
+      max = arguments[i];
+    }
+  }
+  return max;
+}
+console.log(x)
