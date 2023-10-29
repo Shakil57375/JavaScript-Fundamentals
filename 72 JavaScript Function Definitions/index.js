48d2c21e414cce3c236020249aaece72346815b3
Shakil57375 <shakil57375@gmail.com>
2023-10-29 11:41:50 +0600

    arrow function added

--- 72 JavaScript Function Definitions/index.js
+++ 72 JavaScript Function Definitions/index.js
@@ -1,13 +1,26 @@
 // will hosted to the top
-function hostedFunction(){
-    return console.log("object")
+function hostedFunction() {
+  return console.log("object");
 }
 // will not hosted to the top
-const myFunc = function(a,b){
-    // console.log(arguments.length)
-    return a*b
-    // by default a function will return undefined.
+const myFunc = function (a, b) {
+  // console.log(arguments.length)
+  return a * b;
+  // by default a function will return undefined.
+};
+
+console.log(myFunc(3, 3));
+console.dir(myFunc);
+
+// Traditional Anonymous Function
+function hlw(a, b) {
+  let chuck = 42;
+  return a + b + chuck;
 }
 
-console.log(myFunc(3,3))
-console.dir( myFunc)
+// Arrow Function
+(a, b) => {
+  let chuck = 42;
+  return a + b + chuck;
+};
+const x = (x, y) => { return x * y };
