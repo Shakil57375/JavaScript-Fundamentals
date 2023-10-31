5e2db1a38637a90ee8a7d425d0fef029f0f4c99c
Shakil57375 <shakil57375@gmail.com>
2023-10-31 11:26:41 +0600

    defination added

--- 75 Javascript function call/index.js
+++ 75 Javascript function call/index.js
@@ -9,6 +9,7 @@ class myFunction {
 //     firstName = arg1;
 //     lastName = arg2;
 // }
+// ! function invocation.
 // incase of normal function
 // myFunction();
 // incase of constructor function
@@ -16,3 +17,20 @@ const called = new myFunction("shakil", "hossain");
 console.log(called)
 // incase of function inside an object
 // myObject.myFunction();
+// here this.firstName refers to a.firstName.
+const a = {
+    firstName : "Shakil",
+    sleep : function(){
+        console.log(`${this.firstName} is sleeping`)
+    }
+}
+a.sleep()
+// We can assume that a function doesn't require the keyword "window" since it's a hidden object.hea
+//! A function can be considered as a type of method.
+/* window{
+    function eating(){
+        console.log(`shakil is eating`)
+    }
+}
+
+window.eating() */
