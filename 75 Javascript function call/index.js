f4380548c7c06042cb77f95e228758e2ebe4ff6c
Shakil57375 <shakil57375@gmail.com>
2023-10-31 19:15:44 +0600

    call method added

--- 75 Javascript function call/index.js
+++ 75 Javascript function call/index.js
@@ -1,9 +1,9 @@
 // function invocation.
 class myFunction {
-    constructor(arg1, arg2) {
-        this.firstName = arg1;
-        this.lastName = arg2;
-    }
+  constructor(arg1, arg2) {
+    this.firstName = arg1;
+    this.lastName = arg2;
+  }
 }
 // function myFunction(arg1, arg2){
 //     firstName = arg1;
@@ -14,17 +14,17 @@ class myFunction {
 // myFunction();
 // incase of constructor function
 const called = new myFunction("shakil", "hossain");
-console.log(called)
+console.log(called);
 // incase of function inside an object
 // myObject.myFunction();
 // here this.firstName refers to a.firstName.
 const a = {
-    firstName : "Shakil",
-    sleep : function(){
-        console.log(`${this.firstName} is sleeping`)
-    }
-}
-a.sleep()
+  firstName: "Shakil",
+  sleep: function () {
+    console.log(`${this.firstName} is sleeping`);
+  },
+};
+a.sleep();
 // We can assume that a function doesn't require the keyword "window" since it's a hidden object.hea
 //! A function can be considered as a type of method.
 /* window{
@@ -34,3 +34,30 @@ a.sleep()
 }
 
 window.eating() */
+
+/* //! The JavaScript call() Method
+The call() method is a predefined JavaScript method.
+
+It can be used to invoke (call) a method with an owner object as an argument (parameter).
+
+With call(), an object can use a method belonging to another object.
+
+This example calls the fullName method of person, using it on person1: */
+
+const person = {
+  fullName: function () {
+    // return console.log(`${this.firstName + " " + this.lastName}`);
+    return this.firstName + " " + this.lastName
+  },
+};
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+};
+const person2 = {
+  firstName: "Mary",
+  lastName: "Doe",
+};
+
+// This will return "John Doe":
+console.log(person.fullName.call(person1));
