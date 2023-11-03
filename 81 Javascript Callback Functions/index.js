3ef10292b6dea8df58bc12e45f7f2a94ae20c911
Shakil57375 <shakil57375@gmail.com>
2023-11-03 14:48:54 +0600

    callback function added

--- 81 Javascript Callback Functions/index.js
+++ 81 Javascript Callback Functions/index.js
@@ -1,4 +1,8 @@
-function myDisplayer(some) {
+// ! I will call you letter
+// ? What is a callback function?
+// * A callback function passed as an argument to another function.
+// normal function
+/* function myDisplayer(some) {
   console.log(some)
 }
 
@@ -8,4 +12,21 @@ function myCalculator(num1, num2) {
 }
 
 let result = myCalculator(5, 5);
-myDisplayer(result);
+myDisplayer(result); */
+// ! callback function.
+function myDisplayer(some) {
+  console.log(some);
+}
+
+function myCalculator(num1, num2, myCallback) {
+  let sum = num1 + num2;
+  if (myCallback) {
+    return myCallback(sum);
+  } else {
+    return sum;
+  }
+}
+
+const result = myCalculator(5, 5, myDisplayer);
+console.log(result);
+  
