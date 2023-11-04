aa18eba1395c4cd2c99530ddb8a4baf204532347
Shakil57375 <shakil57375@gmail.com>
2023-11-04 11:27:27 +0600

    setTimeout vs setInterval added

--- 82 Javascript Asynchronous Behavior/index.js
+++ 82 Javascript Asynchronous Behavior/index.js
@@ -1,6 +1,17 @@
 /* "I will finish later!"
 Functions running in parallel with other functions are called asynchronous
 A good example is JavaScript setTimeout() */
+//.setTimeout() //executes the code after x seconds.
+//.setInterval() //executes the code **every** x seconds.
+var intervalID = setInterval(function(){
+    console.log("hello")
+}, 1000); // Will alert every second.
+// clearInterval(intervalID); // Will clear the timer.
+
+setTimeout(alert, 1000); // Will alert once, after a second.
+setInterval(function(){ 
+	console.log("Oooo Yeaaa!");
+}, 2000);//run this thang every 2 seconds
 console.log("line 1 code");
 // ? we can pass a full function as an argument.
 // setTimeout(function () {
@@ -16,7 +27,6 @@ function displayer(){
 // ! Wrong: setTimeout(myFunction(), 3000); 
 // ! another way to set callback function.
 
-setTimeout(displayer, 2000)
-
+setInterval(displayer, 2000)
 
 console.log("Line 3 code");
