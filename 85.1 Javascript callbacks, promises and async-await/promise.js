8f3b2be41fec7791bdbbde0e63b1383cde7c03c8
Shakil57375 <shakil57375@gmail.com>
2023-11-05 11:55:05 +0600

    javascript promise added

--- 85.1 Javascript callbacks, promises and async-await/promise.js
+++ 85.1 Javascript callbacks, promises and async-await/promise.js
@@ -14,10 +14,32 @@ const meeting = new Promise((resolve, reject)=>{
     }
 })
 
+
+const addToCalendar = (meetingDetails) =>{
+    const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`
+    return Promise.resolve(calendar)
+}
+
 meeting
+    .then(addToCalendar)
     .then((res)=>{
         console.log(JSON.stringify(res))
     })
     .catch((err)=>{
         console.log(err.message)
+    })
+
+// if you want to run multiple promise at a tile use Promise.all
+
+const promise1 = Promise.resolve("Promise 1 resolved")
+
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("promise 2 resolved")
+    }, 2000);
+});
+
+Promise.all([promise1, promise2])
+    .then(res=>{
+        console.log(res)
     })
