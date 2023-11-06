594bc22116fa22e9e9088769e635bfc0f32cb77b
Shakil57375 <shakil57375@gmail.com>
2023-11-06 12:31:25 +0600

    onclick function added on click

--- 94 DOM Evenets/index.js
+++ 94 DOM Evenets/index.js
@@ -1,9 +1,13 @@
-function changeText(){
-    const header = document.getElementById("heading")
-    header.innerHTML = "hello world"
+function changeText() {
+  const header = document.getElementById("heading");
+  header.innerHTML = "hello world";
 }
 
-function changeTextWithThis(heading){
-    heading.innerHTML = "changed"
-    
+function changeTextWithThis(heading) {
+  heading.innerHTML = "changed";
 }
+
+const btn = document.getElementById("btn");
+btn.onclick = function () {
+  console.log(Date());
+};
