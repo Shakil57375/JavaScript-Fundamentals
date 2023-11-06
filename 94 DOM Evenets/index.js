f04e058f6111765e73830c0ff06eeb0bedc1aefe
Shakil57375 <shakil57375@gmail.com>
2023-11-06 18:29:58 +0600

    onmousedown and onmousup event added

--- 94 DOM Evenets/index.js
+++ 94 DOM Evenets/index.js
@@ -8,6 +8,22 @@ function changeTextWithThis(heading) {
 }
 
 const btn = document.getElementById("btn");
-btn.onclick = function () {
+btn.onclick = setDate;
+function setDate() {
   console.log(Date());
-};
+}
+
+function upperCase() {
+  let x = document.getElementById("fname");
+  x.value = x.value.toUpperCase();
+}
+
+function mDown(obj) {
+  obj.style.backgroundColor = "#1ec5e5";
+  obj.innerHTML = "Release Me";
+}
+
+function mUp(obj) {
+  obj.style.backgroundColor = "#D94A38";
+  obj.innerHTML = "Thank You";
+}
