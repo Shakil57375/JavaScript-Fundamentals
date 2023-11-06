064111177067d361ec055c0cd0df5a04364621c6
Shakil57375 <shakil57375@gmail.com>
2023-11-06 12:19:20 +0600

    change with the external function added

--- 94 DOM Evenets/index.js
+++ 94 DOM Evenets/index.js
@@ -2,3 +2,8 @@ function changeText(){
     const header = document.getElementById("heading")
     header.innerHTML = "hello world"
 }
+
+function changeTextWithThis(heading){
+    heading.innerHTML = "changed"
+    
+}
