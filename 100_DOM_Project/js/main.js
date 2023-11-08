af04602ed0692f40e44c8defbaa13e5aee5b6646
Shakil57375 <shakil57375@gmail.com>
2023-11-08 12:54:47 +0600

    show and remove functionalities added

--- 100_DOM_Project/js/main.js
+++ 100_DOM_Project/js/main.js
@@ -9,14 +9,14 @@ const loadMilestoneData = () => {
       <div class="checkbox">
         <input type="checkbox" />
       </div>
-      <div>
+      <div onclick = "openMilestone(this)">
         <p>
           ${milestone.name}
           <span><i class="fas fa-chevron-down"></i></span>
         </p>
       </div>
     </div>
-    <div class="hid den_panel">
+    <div class="hidden_panel">
         ${milestone.modules.map(module =>{
          return `<div class="module border-b">
           <p>${module.name}</p>
@@ -27,5 +27,15 @@ const loadMilestoneData = () => {
   </div>`;
   }).join("")}`;
 };
+
+const openMilestone = (milestoneElements) =>{
+  const currentPanel = milestoneElements.parentNode.nextElementSibling;
+  const showPanel = document.querySelector(".show")
+  if(!currentPanel.classList.contains("show") && showPanel ){
+    showPanel.classList.remove("show")
+  }
+
+  currentPanel.classList.toggle("show");
+}
 
 loadMilestoneData();
