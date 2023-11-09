9b9b69afd125fd4869ad5d481612ba28c496de6a
Shakil57375 <shakil57375@gmail.com>
2023-11-09 20:10:14 +0600

    programming hero hacked!

--- 100_DOM_Project/js/main.js
+++ 100_DOM_Project/js/main.js
@@ -1,13 +1,16 @@
-const mileStoneData = JSON.parse(data).data;
-console.log(mileStoneData);
+const milestoneData = JSON.parse(data).data;
+console.log(milestoneData);
+
 const loadMilestoneData = () => {
   const milestoneDetails = document.querySelector(".milestones");
-  milestoneDetails.innerHTML = `${mileStoneData
+  milestoneDetails.innerHTML = `${milestoneData
     .map((milestones) => {
-      return `<div class="milestone border-b">
+      return `<div class="milestone border-b " id="${milestones._id}">
     <div class="flex">
-      <div class="checkbox"><input type="checkbox" /></div>
-      <div onclick = "displayModules(this, ${milestones._id})">
+      <div class="checkbox"><input type="checkbox" onclick = "ModuleDoneList(this, ${
+        milestones._id
+      })" /></div>
+      <div onclick = "showModules(this, ${milestones._id})">
         <p>
           ${milestones.name}
           <span><i class="fas fa-chevron-down"></i></span>
@@ -28,18 +31,50 @@ const loadMilestoneData = () => {
     .join("")}`;
 };
 
-const displayModules = (milestoneHeader, id) => {
+const showModules = (milestoneHeader, id) => {
   const currentPanel = milestoneHeader.parentNode.nextElementSibling;
-  const active = document.querySelector(".active")
-  if(!milestoneHeader.classList.contains("active") && active){
-    active.classList.remove("active")
-  }
-  milestoneHeader.classList.toggle("active")
 
   const showPanel = document.querySelector(".show");
   if (!currentPanel.classList.contains("show") && showPanel) {
     showPanel.classList.remove("show");
   }
   currentPanel.classList.toggle("show");
+
+  const active = document.querySelector(".active");
+  if (!milestoneHeader.classList.contains("active") && active) {
+    active.classList.remove("active");
+  }
+  milestoneHeader.classList.toggle("active");
+
+  milestoneImage(id);
 };
+
+const milestoneImage = (id) => {
+  const milestoneImg = document.querySelector(".milestoneImage");
+  const milestoneTitle = document.querySelector(".title");
+  const milestoneDetails = document.querySelector(".details");
+  milestoneImg.style.opacity = "0";
+  milestoneImg.src = milestoneData[id].image;
+  milestoneTitle.innerHTML = milestoneData[id]?.name;
+  milestoneDetails.innerHTML = milestoneData[id]?.description;
+};
+
+const milestoneImg = document.querySelector(".milestoneImage");
+milestoneImg.onload = function () {
+  this.style.opacity = "1";
+};
+
+const ModuleDoneList = (doneMileStone, id) => {
+  const finishedMileStone = document.querySelector(".doneList");
+  const milestones = document.querySelector(".milestones");
+  const item = document.getElementById(id)
+  if (doneMileStone.checked) {
+    finishedMileStone.appendChild(item);
+    milestones.removeChild(item);
+  } else {
+    finishedMileStone.removeChild(item);
+    milestones.appendChild(item);
+  }
+};
+
 loadMilestoneData();
