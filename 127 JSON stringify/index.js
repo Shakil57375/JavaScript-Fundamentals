5a24fff0567f4977a1fb9a6520a6648522201622
Shakil57375 <shakil57375@gmail.com>
2023-11-12 19:11:59 +0600

    json stringify  added

---
+++ 127 JSON stringify/index.js
@@ -0,0 +1,8 @@
+const obj = {name: "John", age: 30, city: "New York"};
+// convert it to json
+const json = JSON.stringify(obj)
+console.log(json)
+const arr = ["John", "Peter", "Sally", "Jane"];
+// convert array to json
+const arrStringify = JSON.stringify(arr)
+console.log(arrStringify)
