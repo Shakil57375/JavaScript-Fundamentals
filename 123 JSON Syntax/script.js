e71f3417d1aa182a235ad4278627a640fd467c31
Shakil57375 <shakil57375@gmail.com>
2023-11-12 18:42:49 +0600

    json syntax added

---
+++ 123 JSON Syntax/script.js
@@ -0,0 +1,17 @@
+const person = '{"name" : "Shakil", "age" : 21, "skills" : ["programming", "learner"], "birthday" : new Date(23-4-2002)}' // can't add new date.
+
+/* JSON Values
+In JSON, values must be one of the following data types:
+
+a string
+a number
+an object
+an array
+a boolean
+null
+In JavaScript values can be all of the above, plus any other valid JavaScript expression, including:
+
+a function
+a date
+undefined
+In JSON, string values must be written with double quotes: */
