38660e914d2c7fe515c0cd708d08d97bc8de2efe
Shakil57375 <shakil57375@gmail.com>
2024-01-02 21:54:30 +0600

    updated

--- Think In JavaScript Way/4 Javascript Prototype/script.js
+++ Think In JavaScript Way/4 Javascript Prototype/script.js
@@ -1,15 +1,21 @@
+const personMethods = {
+  eat() {
+    console.log(`Person is playing`);
+  },
+  play() {
+    console.log(`Person is playing`);
+  },
+};
 function Person(name, age) {
-    let person = {};
-    person.name = name;
-    person.age = age;
-    person.eat = function(){
-        console.log(`person is eating`)
-    }
-    person.sleep = function(){
-        console.log(`person is sleeping`)
-    }
-    return person
+  let person = Object.create(personMethods);
+  person.name = name;
+  person.age = age;
+  return person;
 }
 
-const sakib = Person("Sakib", 35)
-const tamim = Person("Tamim", 36)
+const sakib = Person("Sakib", 35);
+sakib.play();
+const tamim = Person("Tamim", 36);
+
+function test() {}
+
