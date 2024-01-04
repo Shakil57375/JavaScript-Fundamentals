045418c9ed17665994c03b11fbafab64daaa2eaf
Shakil57375 <shakil57375@gmail.com>
2024-01-04 21:19:12 +0600

    difference between var let and const

---
+++ Need_To_Know_For_React/function.js
@@ -0,0 +1,24 @@
+// Function declaration
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Using the function
+  const sum = add(3, 4);
+  console.log(sum); // Output: 7
+  
+  // Function expression
+const subtract = function(a, b) {
+    return a - b;
+  };
+  
+  // Using the function
+  const result = subtract(8, 5);
+  console.log(result); // Output: 3
+  
+  // Arrow function
+const multiply = (a, b) => a * b;
+
+// Using the function
+const product = multiply(2, 6);
+console.log(product); // Output: 12
