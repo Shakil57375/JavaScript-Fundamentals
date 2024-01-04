682785c1741409fc93c7e8918e8809899e80708b
Shakil57375 <shakil57375@gmail.com>
2024-01-04 21:41:06 +0600

    difference between var let and const

--- Need_To_Know_For_React/function.js
+++ Need_To_Know_For_React/function.js
@@ -1,24 +1,39 @@
 // Function declaration
 function add(a, b) {
-    return a + b;
-  }
-  
-  // Using the function
-  const sum = add(3, 4);
-  console.log(sum); // Output: 7
-  
-  // Function expression
-const subtract = function(a, b) {
-    return a - b;
-  };
-  
-  // Using the function
-  const result = subtract(8, 5);
-  console.log(result); // Output: 3
-  
-  // Arrow function
+  return a + b;
+}
+
+// Using the function
+const sum = add(3, 4);
+console.log(sum); // Output: 7
+
+// Function expression
+const subtract = function (a, b) {
+  return a - b;
+};
+
+// Using the function
+const result = subtract(8, 5);
+console.log(result); // Output: 3
+
+// Arrow function
 const multiply = (a, b) => a * b;
 
 // Using the function
 const product = multiply(2, 6);
 console.log(product); // Output: 12
+/* document.getElementById("myButton").addEventListener("click", function () {
+    alert("Button clicked!");
+}); */
+
+const hlw = () =>{
+    console.log("print hlw")
+}
+
+// ! another way to handle it.
+
+/* document.getElementById("myButton").addEventListener("click", hlw); */
+
+document.getElementById('myButton').addEventListener('click', function(event) {
+    alert('Button clicked! X: ' + event.clientX + ', Y: ' + event.clientY);
+  });
