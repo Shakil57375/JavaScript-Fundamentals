0697e0e87702fb94be2dc3b28964b921fd8c7154
Shakil57375 <shakil57375@gmail.com>
2024-01-05 20:27:32 +0600

    nullish_coalescing_operator

--- Need_To_Know_For_React/nullish_coalescing_operator.js
+++ Need_To_Know_For_React/nullish_coalescing_operator.js
@@ -3,7 +3,7 @@ Expression:
 	// Left ?? Right
 // if left is null or undefined , then Right will be the value
 
-let value = null ?? "Oops.. null or undefined";
+var value = null ?? "Oops.. null or undefined";
 console.log(value) //Oops.. null or undefined
 
 value = undefined ?? "Oops.. null or undefined";
@@ -15,3 +15,7 @@ console.log(value) // 25
 value = "" ?? "Oops.. null or undefined";
 console.log(value) // ""
 8
+
+const val = false
+const res = val  || "hlw"
+console.log(res)
